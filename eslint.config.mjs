468c9e28de54f6c3f1f27621a03d63629b8bddab
Vishnu Mouli <vishnumouli0@gmail.com>
2025-03-18 20:28:23 +0530

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,59 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      //   "no-console": [
+      //     "error",
+      //     {
+      //       allow: ["info", "warn", "error"],
+      //     },
+      //   ],
+
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+
+      "@typescript-eslint/no-explicit-any": [
+        "error",
+        {
+          fixToUnknown: true,
+        },
+      ],
+
+      "@typescript-eslint/no-unused-vars": [
+        "off",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      "react/no-unescaped-entities": "off",
+      semi: ["error", "always"],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "no-trailing-spaces": "error",
+      quotes: ["error", "double"],
+    },
+  },
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,59 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	productionBrowserSourceMaps: false,
+	images: {
+	  remotePatterns: [
+		{ protocol: "https", hostname: "res.cloudinary.com" },
+		{ protocol: "https", hostname: "img.clerk.com" },
+		{ protocol: "https", hostname: "images.unsplash.com" },
+	  ],
+	  minimumCacheTTL: 86400,
+	  deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+	  imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+	},
+	reactStrictMode: true,
+	async headers() {
+	  return [
+		{
+		  source: "/api/:path*",
+		  headers: [
+			{ key: "Access-Control-Allow-Credentials", value: "true" },
+			{ key: "Access-Control-Allow-Origin", value: "*" },
+			{
+			  key: "Access-Control-Allow-Methods",
+			  value: "GET,DELETE,PATCH,POST,PUT",
+			},
+			{
+			  key: "Access-Control-Allow-Headers",
+			  value:
+				"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+			},
+		  ],
+		},
+		{
+		  source: "/:all*(svg|jpg|png)",
+		  headers: [
+			{
+			  key: "Cache-Control",
+			  value: "public, max-age=31536000, immutable",
+			},
+			{
+			  key: "Content-Encoding",
+			  value: "br", // Brotli compression for better performance
+			},
+		  ],
+		},
+		{
+		  source: "/_next/static/:path*",
+		  headers: [
+			{
+			  key: "Cache-Control",
+			  value: "public, max-age=31536000, immutable",
+			},
+		  ],
+		},
+	  ];
+	},
+  };
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+  export default nextConfig;
